bf087a26cc6d7d539561616c8b82552b533a9965
sharanreddy111 <96769819+sharanreddy111@users.noreply.github.com>
2022-03-02 21:15:52 +0530

    10.Interfaces Assignment

---
+++ 10.Interfaces Assignment/src/com/jala/interfaces/InterfaceOneMethod.java
@@ -0,0 +1,27 @@
+/**
+ * @author JALA Academy
+ * @author K.Sharan Simha Reddy
+ *
+ * 01. Create an interface with only one method and implement it in a class. Call the method
+ * implemented.
+ */
+package com.jala.interfaces;
+
+//An interface is declared by using the interface keyword.
+interface Jala_01 {
+    //An abstract method doesn't have its body, and they are public and abstract
+    void oneMethod();
+}
+
+public class InterfaceOneMethod implements Jala_01 {
+    @Override
+    public void oneMethod() {
+        System.out.println("This is a method");
+    }
+
+    public static void main(String[] args) {
+        InterfaceOneMethod if1 = new InterfaceOneMethod();
+        //Calling the method implemented
+        if1.oneMethod();
+    }
+}
